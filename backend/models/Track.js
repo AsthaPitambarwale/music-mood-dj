2ddec6456867a727b157efa0e8821694bc80382f
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2025-12-04 02:37:45 +0530

    Update Track.js

--- backend/models/Track.js
+++ backend/models/Track.js
@@ -7,7 +7,6 @@ const trackSchema = new mongoose.Schema(
     url: { type: String, required: true }, // uploaded file URL
     publicId: { type: String },
     duration: { type: Number },
-    mood: { type: String },
     playCount: { type: Number, default: 0 } // REQUIRED for top tracks + mix
   },
   { timestamps: true }
