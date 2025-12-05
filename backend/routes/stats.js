c812ab2aeb62d87f6dbe6586a4c687030f8c9ff1
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2025-12-05 22:12:06 +0530

    Update stats.js

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getTopTracks, incrementPlayCount } from "../controllers/statsController.js";
+import { 
+  getTopTracks, 
+  incrementPlayCount 
+} from "../controllers/statsController.js";
 
 const router = express.Router();
 
 router.get("/top", getTopTracks);
+
 router.post("/play/:id", incrementPlayCount);
 
 export default router;
-
