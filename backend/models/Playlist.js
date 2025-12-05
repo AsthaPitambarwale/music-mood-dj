85ac8d8ac59d4c83acd22c51df37dfe611367641
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2025-12-05 21:50:15 +0530

    Update Playlist.js

--- backend/models/Playlist.js
+++ backend/models/Playlist.js
@@ -3,16 +3,21 @@ import mongoose from "mongoose";
 const playlistSchema = new mongoose.Schema(
   {
     mood: { type: String, required: true },
+
     tracks: [
       {
-        trackId: { type: mongoose.Schema.Types.ObjectId, ref: "Track" },
-        order: Number,
-        weight: Number,
+        trackId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Track",
+          required: true,
+        },
+        order: { type: Number, default: 1 },
+        weight: { type: Number, default: 1 },
       },
     ],
   },
   { timestamps: true }
 );
 
-const Playlist = mongoose.model("Playlist", playlistSchema);
-export default Playlist;
+export default mongoose.models.Playlist ||
+  mongoose.model("Playlist", playlistSchema);
