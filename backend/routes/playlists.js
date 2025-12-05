cde3c5312944de2dda18b79b0a4720de47bc5562
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2025-12-05 22:13:17 +0530

    Update playlists.js

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createPlaylist, getPlaylist } from "../controllers/playlistController.js";
+import { 
+  createPlaylist, 
+  getPlaylist 
+} from "../controllers/playlistController.js";
 
 const router = express.Router();
 
 router.post("/", createPlaylist);
+
 router.get("/:id", getPlaylist);
 
 export default router;
